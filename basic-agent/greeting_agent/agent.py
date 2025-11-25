a0c1fd47337e6df2dcbed86835463fff77721df7
Ryan Iqbal Hermawan <ryan.iqbalh@gmail.com>
2025-11-25 22:46:17 +0700

    Update agent.py

--- basic-agent/greeting_agent/agent.py
+++ basic-agent/greeting_agent/agent.py
@@ -1,12 +1,12 @@
 from google.adk.agents import Agent
 
 root_agent = Agent(
-    name="greeting_agent",
-    # https://ai.google.dev/gemini-api/docs/models
-    model="gemini-2.0-flash",
+    name="greeting_agent", # The agent name must match the folder name.
+    model="gemini-2.0-flash", # https://ai.google.dev/gemini-api/docs/models
     description="Greeting agent",
     instruction="""
     You are a helpful assistant that greets the user. 
     Ask for the user's name and greet them by name.
     """,
+
 )
